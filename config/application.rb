f909ef27b0781f72206c617beded4de3a9a93d54
Valéria Parajara <valeriaparajara@gmail.com>
2014-04-29 00:02:48 -0300

    [#006] Ajustes no layout padrão da app

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Emprestei
   class Application < Rails::Application
+
+    config.assets.paths << "#{Rails}/vendor/assets/stylesheets/fonts"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
